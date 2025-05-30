f66d4189fa68dd095813c8221878dcba44c49639
KrGozme <kildergozmequispe@gmail.com>
2025-05-30 12:17:50 -0500

    prueba 2

---
+++ config/config example.php
@@ -0,0 +1,13 @@
+<?php
+
+const BD_HOST = 'localhots';
+const BD_NAME = '';
+const BD_USER = '';
+const BD_PASSWORD = '';
+const BD_CHARSET = 'utf8';
+
+// ingrese link del proyecto
+
+const BASE_URL = '';
+
+?>
