7b29a78b76e16cffe3ae349017b98d3a2a26ed2a
KrGozme <kildergozmequispe@gmail.com>
2025-06-27 12:58:43 -0500

    ULTIMO

---
+++ view/function/user.js
@@ -0,0 +1,65 @@
+function validar_form() {
+    let nro_documento = document.getElementById("nro_identidad").value;
+    let razon_social = document.getElementById("razon_social").value;
+    let telefono = document.getElementById("telefono").value;
+    let correo = document.getElementById("correo").value;
+    let departamento = document.getElementById("departamento").value;
+    let provincia = document.getElementById("provincia").value;
+    let distrito = document.getElementById("distrito").value;
+    let cod_postal = document.getElementById("cod_postal").value;
+    let direccion = document.getElementById("direccion").value;
+    let rol = document.getElementById("rol").value;
+
+    if (nro_documento == "" || razon_social == "" || telefono == "" || correo == "" || departamento == "" || provincia == "" || distrito == "" || cod_postal == "" || direccion == "" || rol == "") {
+        Swal.fire({
+            //alerta de error al momento de poner campos vacios 
+            icon: "error",
+            title: "Error existen campos vacios",
+            text: "No seas Gil",
+        });
+        return;
+    }
+    /*//alerta de registrado con exito
+    Swal.fire({
+        title: "Registrado con exito",
+        text: "Ya fuiste",
+        icon: "success",
+        draggable: true
+    });*/
+
+    registrarUsuario();
+}
+
+if (document.querySelector('#frm_user')) {
+    // valida que se envie el formulario 
+    let frm_user = document.querySelector('#frm_user');
+    frm_user.onsubmit = function (e) {
+        e.preventDefault()
+        validar_form();
+    }
+}
+
+async function registrarUsuario() {
+    try {
+        //capturar campos de formulario (HTML)
+        const datos = new FormData(frm_user);
+        //enviar datos al controlador
+        let respuesta = await fetch(base_url + 'control/UsuarioController.php?tipo=registrar',{
+            method: 'POST',
+            mode: 'cors',
+            cache: 'no-cache',
+            body: datos
+        });
+        let json = await respuesta.json();
+        // validamos que json.status sea = true
+        if (json.status) {
+            alert(json.msg);
+            document.getElementById('frm_user').reset();
+        }else {
+            alert(json.msg);
+        }
+
+    } catch (error) {
+        console.log("Error al registrar usuario:"+ error)
+    }
+}
---
+++ view/new-user.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>view/bootstrap/css/bootstrap.min.css">
+    <title>Kilder</title>
+
+    <script>
+        const base_url = '<?php echo BASE_URL; ?>';
+    </script>
+
+
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Logo</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">users</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">products</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">categories</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">clients</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">shops</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">save</a>
+                </ul>
+                <form class="d-flex" role="search">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Dropdown
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </form>
+            </div>
+        </div>
+    </nav>
+    <div class="conteiner-fluid">
+        <div class="card">
+            <h5 class="card-header">titulo</h5>
+            <form id="frm_user" action="" method="">
+                <div class="card-body">
+                    <div class="mb-3 row">
+                        <label for="nro_identidad" class="col-sm-4 col-form-label">Nro de Documento:</label>
+                        <div class="col-sm-8">
+                            <input type="number" class="form-control" id="nro_identidad" name="nro_identidad" placeholder="Numero de DNI" required>
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="razon_social" class="col-sm-4 col-form-label">Razon Social:</label>
+                        <div class="col-sm-8">
+                            <input type="text" class="form-control" id="razon_social" name="razon_social" placeholder="Razon Social" required>
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="telefono" class="col-sm-4 col-form-label">Telefono:</label>
+                        <div class="col-sm-8">
+                            <input type="number" class="form-control" id="telefono" name="telefono" placeholder="Numero de telefono" required>
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="correo" class="col-sm-4 col-form-label">Correo Electronico:</label>
+                        <div class="col-sm-8">
+                            <input type="email" class="form-control" id="correo" name="correo" placeholder="Correo Electronico" required>
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="departamento" class="col-sm-4 col-form-label">Departamento:</label>
+                        <div class="col-sm-8">
+                            <input type="text" class="form-control" id="departamento" name="departamento" placeholder="Departamento" required>
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="provincia" class="col-sm-4 col-form-label">Provincia:</label>
+                        <div class="col-sm-8">
+                            <input type="text" class="form-control" id="provincia" name="provincia" placeholder="Provincia" required>
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="distrito" class="col-sm-4 col-form-label">Distrito:</label>
+                        <div class="col-sm-8">
+                            <input type="text" class="form-control" id="distrito" name="distrito" placeholder="Distrito" required>
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="cod_postal" class="col-sm-4 col-form-label">Codigo Postal:</label>
+                        <div class="col-sm-8">
+                            <input type="number" class="form-control" id="cod_postal" name="cod_postal" placeholder="Codigo Postal" required>
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="direccion" class="col-sm-4 col-form-label">Direccion:</label>
+                        <div class="col-sm-8">
+                            <input type="text" class="form-control" id="direccion" name="direccion" placeholder="Direccion" required>
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="rol" class="col-sm-4 col-form-label">Rol:</label>
+                        <div class="col-sm-8">
+                            <select class="form-select" id="rol" name="rol" required>
+                                <option selected>Opsiones</option>
+                                <option value="1">admi</option>
+                                <option value="2">Gerente</option>
+                                <option value="3">Trabajador</option>
+                            </select>
+                        </div>
+                    </div>
+                    <button type="submit" class="btn btn-success">Registrar</button>
+                    <button type="reset" class="btn btn-info">Limpiar</button>
+                    <button type="button" class="btn btn-danger">Cancelar</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+<script src="<?php echo BASE_URL; ?>view/function/user.js"></script>
+<script src="<?php echo BASE_URL; ?>view/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</html>
