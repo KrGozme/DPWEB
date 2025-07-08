d32bc1bb393bf484c6b5a81f66f556992c0d7d74
KrGozme <kildergozmequispe@gmail.com>
2025-07-08 12:58:16 -0500

    hoy

--- view/categories.php
+++ view/categories.php
@@ -1,68 +1,4 @@
-<!DOCTYPE html>
-<html lang="es">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?php echo BASE_URL; ?>view/bootstrap/css/bootstrap.min.css">
-    <title>Registrar Categoría</title>
-
-    <script>
-        const base_url = '<?php echo BASE_URL; ?>';
-    </script>
-</head>
-
-<body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Logo</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="<?php echo BASE_URL; ?>new-user">users</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">products</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">categories</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">clients</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">shops</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">save</a>
-                </ul>
-                <form class="d-flex" role="search">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </form>
-            </div>
-        </div>
-    </nav>
-
+<!-- Cuerpo de la pagina-->
     <div class="container mt-4">
         <div class="card">
             <h5 class="card-header">Registrar Categoría</h5>
@@ -87,10 +23,5 @@
             </form>
         </div>
     </div>
-</body>
-
+<!-- Fin de cuerpo de la pagina-->
 <script src="<?php echo BASE_URL; ?>view/function/Categories.js"></script>
-<script src="<?php echo BASE_URL; ?>view/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-
-</html>
