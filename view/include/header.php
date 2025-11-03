957d79ae97e835527029a53c0276e29bbdf78175
KrGozme <kildergozmequispe@gmail.com>
2025-11-03 12:45:30 -0500

    .

--- view/include/header.php
+++ view/include/header.php
@@ -37,7 +37,7 @@
 
         <div class="user-info">
             <span>Admin</span>
-            <img src="https://m.media-amazon.com/images/M/MV5BZjA0MDgyYmItNzkzMC00OTM2LThlYzktMWMxZWU3ZGNkNDI3XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg" alt="User">
+            <img src="<?php echo BASE_URL; ?>view/img/admi.png" alt="User">
         </div>
     </header>
 
